f6fa8f83f11fad9cea3bdcc0cc8640858a9b7dd8
Van Vu <vanvn.vu@gmail.com>
2023-04-20 10:38:32 +0700

    Added try catch beginner

---
+++ trycatch.ts
@@ -0,0 +1,25 @@
+// let num = 5;
+// let de_num = 2;
+
+// try {
+//   if (de_num === 0) {
+//     throw `Divided by Zero error`;
+//   } else {
+//     let sol = num / de_num;
+//     console.log(sol);
+//   }
+// } catch (e) {
+//   console.log(`Error: ${e}`);
+// }
+
+// try {
+//   ab();
+// } catch (e) {
+//   console.log(`Error: ${e}`);
+// }
+
+try {
+  eval("alert('ES6 Exception Handling)");
+} catch (e) {
+  console.log(`Error: ${e}`);
+}
